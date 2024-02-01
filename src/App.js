4abf78ad835b7584a16de32f1666c6bbb2f41bda
Min3th <minethweerasinghe@gmail.com>
2024-02-01 16:10:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import sound from "./sounds/best_alarm.mp3"
+import { useState, useEffect } from 'react';
+import {format, parse} from "date-fns";
 
 function App() {
+
+
+  const [hours,setHours] = useState('');
+  const [minutes,setMinutes] = useState('');
+  let activeTime = '';
+  let audioElement =new Audio(sound)
+
+  // useEffect(()=>{
+  //   play()
+  // },[value])
+
+
+  const currentTime = new Date();
+  const alarm =(activeTime) =>{
+    const now = new Date()
+    
+    const [enteredHours,enteredMinutes] = activeTime.split(':');
+
+    const targetHours = parseInt(enteredHours,10) - now.getHours();
+    const targetMinutes = parseInt(enteredMinutes,10) - now.getMinutes();
+    const targetTime = (targetHours*60*60) + (targetMinutes*60);
+    console.log(targetTime);
+    
+    setTimeout(()=>{
+      audioElement.play();
+      console.log('working')
+    },targetTime*1000);
+    
+  }
+  
+  function Stop(){
+    audioElement.pause();
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    activeTime = `${hours}:${minutes}`
+    alarm(activeTime);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+        id ="hour-input"
+        type='number'
+        required
+        placeholder='Hours'
+        value={hours}
+        onChange={(e)=>setHours(e.target.value)}
+        />
+        <input
+        id ="minutes-input"
+        type='number'
+        placeholder='minutes'
+        required
+        value={minutes}
+        onChange={(e)=>setMinutes(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+     
+        
+        
+
+
+      <button onClick={Stop}>
+        Press to Stop
+      </button>
+      
     </div>
   );
 }
